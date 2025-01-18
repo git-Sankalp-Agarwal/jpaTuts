5810f1186293ea8252e82a92d33fd76a7cba3f94
Sankalp Agarwal <sankalpagarwal1304@gmail.com>
2025-01-18 20:24:07 +0530

    Update ProductController base URL mapping

--- src/main/java/com/learnsbt/jpaTutorial/jpaTuts/controllers/ProductController.java
+++ src/main/java/com/learnsbt/jpaTutorial/jpaTuts/controllers/ProductController.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/productDs")
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
